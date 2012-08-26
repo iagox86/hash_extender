7eefd8e2e69a4de5eb68d7b8f906fd8e26ba9478
Ron <ron@skullsecurity.net>
2012-08-26 00:08:54 -0500

    SHA1 seems to be working in its entirety now - it passes all tests.

--- hash_extender.c
+++ hash_extender.c
@@ -78,15 +78,19 @@ uint8_t *sha1_append_data(uint8_t *data, size_t data_length, size_t secret_lengt
   memmove(result, data, data_length);
   *new_length = data_length;
 
-  /* SHA1 uses a 1 bit (0x80) followed by enough 0 bits to get 4 bytes from the end of the buffer. */
+
   result[(*new_length)++] = 0x80;
-  while(((*new_length + 4 + secret_length) % SHA1_BLOCK) != 0)
+  while(((*new_length + secret_length) % SHA1_BLOCK) != 56)
     result[(*new_length)++] = 0x00;
 
-  /* Convert the original length to bits */
+  /* Convert the original length to bits so we can append it. */
   bit_length = (secret_length + data_length) * 8;
 
   /* Set the last 4 bytes of result to the new length. */
+  result[(*new_length)++] = 0;
+  result[(*new_length)++] = 0;
+  result[(*new_length)++] = 0;
+  result[(*new_length)++] = 0;
   result[(*new_length)++] = (bit_length >> 24) & 0x000000FF;
   result[(*new_length)++] = (bit_length >> 16) & 0x000000FF;
   result[(*new_length)++] = (bit_length >>  8) & 0x000000FF;
@@ -116,13 +120,15 @@ void sha1_gen_signature_evil(size_t secret_length, size_t data_length, uint8_t o
 
   SHA1_Init(&c);
 
-  /* Round the total bytes we need to add to the next highest multiple of SHA_DIGEST_LENGTH. */
-  original_data_length = (((secret_length + data_length) / SHA1_BLOCK) * SHA1_BLOCK) + SHA1_BLOCK;
+  /* We need to add bytes equal to the original size of the message, plus
+   * padding. The reason we add 8 is because the padding is based on the
+   * (length % 56) (8 bytes before a full block size). */
+  original_data_length = (((secret_length + data_length + 8) / SHA1_BLOCK) * SHA1_BLOCK) + SHA1_BLOCK;
   for(i = 0; i < original_data_length; i++)
     SHA1_Update(&c, "A", 1);
 
   /* Restore the original context (letting us start from where the last hash left off). */
-  /* . TODO: is ntonl() the appropriate function here? */
+  /* TODO: is ntonl() the appropriate function here? Will this work on a big-endian system? */
   c.h0 = htonl(((int*)original_signature)[0]);
   c.h1 = htonl(((int*)original_signature)[1]);
   c.h2 = htonl(((int*)original_signature)[2]);
@@ -213,7 +219,7 @@ void test_different_length_secret()
 
   size_t i;
 
-  for(i = 0; i < 175; i++)
+  for(i = 0; i < 75; i++)
   {
     /* Get the original signature. */
     sha1_gen_signature(secret, i, data, strlen((char*)data), original_signature);
@@ -226,7 +232,80 @@ void test_different_length_secret()
 
     /* Check the new signature. */
     if(!sha1_check_signature(secret, i, new_data, new_length, new_signature))
+    {
       printf("Length %ld: Failed!\n", i);
+      printf("  signature + data = %d\n", (int)(strlen((char*)data) + i));
+    }
+  }
+
+  free(new_data);
+}
+
+void test_different_length_data()
+{
+  uint8_t *secret    = (uint8_t*)"SECRET";
+  uint8_t *data      = (uint8_t*)"AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA";
+  uint8_t *append    = (uint8_t*)"APPENDZ0R";
+  uint8_t *new_data;
+  size_t  new_length;
+
+  uint8_t original_signature[SHA_DIGEST_LENGTH];
+  uint8_t new_signature[SHA_DIGEST_LENGTH];
+
+  size_t i;
+
+  for(i = 0; i < 75; i++)
+  {
+    /* Get the original signature. */
+    sha1_gen_signature(secret, strlen((char*)secret), data, i, original_signature);
+
+    /* Create the new data. */
+    new_data = sha1_append_data(data, i, strlen((char*)secret), append, strlen((char*)append), &new_length);
+
+    /* Generate an evil signature with the data appended. */
+    sha1_gen_signature_evil(strlen((char*)secret), i, original_signature, append, strlen((char*)append), new_signature);
+
+    /* Check the new signature. */
+    if(!sha1_check_signature(secret, strlen((char*)secret), new_data, new_length, new_signature))
+    {
+      printf("Length %ld: Failed!\n", i);
+      printf("  signature + data = %d\n", (int)(strlen((char*)secret) + i));
+    }
+  }
+
+  free(new_data);
+}
+
+void test_different_length_append()
+{
+  uint8_t *secret    = (uint8_t*)"SEKRET";
+  uint8_t *data      = (uint8_t*)"DATA";
+  uint8_t *append    = (uint8_t*)"AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA";
+  uint8_t *new_data;
+  size_t  new_length;
+
+  uint8_t original_signature[SHA_DIGEST_LENGTH];
+  uint8_t new_signature[SHA_DIGEST_LENGTH];
+
+  size_t i;
+
+  for(i = 0; i < 75; i++)
+  {
+    /* Get the original signature. */
+    sha1_gen_signature(secret, strlen((char*)secret), data, strlen((char*)data), original_signature);
+
+    /* Create the new data. */
+    new_data = sha1_append_data(data, strlen((char*)data), strlen((char*)secret), append, i, &new_length);
+
+    /* Generate an evil signature with the data appended. */
+    sha1_gen_signature_evil(strlen((char*)secret), strlen((char*)data), original_signature, append, i, new_signature);
+
+    /* Check the new signature. */
+    if(!sha1_check_signature(secret, strlen((char*)secret), new_data, new_length, new_signature))
+    {
+      printf("Length %ld: Failed!\n", i);
+      printf("  signature + data = %d\n", (int)(strlen((char*)data) + i));
+    }
   }
 
   free(new_data);
@@ -246,6 +325,12 @@ int main()
   printf("\n-----------------------------------------------------\n\n");
   printf("test_different_length_secret:\n");
   test_different_length_secret();
+  printf("\n-----------------------------------------------------\n\n");
+  printf("test_different_length_data:\n");
+  test_different_length_data();
+  printf("\n-----------------------------------------------------\n\n");
+  printf("test_different_length_append:\n");
+  test_different_length_append();
 
   return 0;
 }
