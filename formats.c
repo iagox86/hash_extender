f690cf75d3e89a8df775128a39d64994dab64986
Ron <ron@skullsecurity.net>
2012-09-16 03:13:50 -0500

    A lot of code cleanup in functions.c/functions.h. Primarily: testing for (and properly handling) side-cases, as well as comments.

--- formats.c
+++ formats.c
@@ -11,6 +11,8 @@
 
 #include "formats.h"
 
+/* I usually hate prototypes, but I want the array of function pointers at the
+ * top so I don't really have a choice in this case. */
 static uint8_t *encode_none(uint8_t *data, uint64_t data_length, uint64_t *out_length);
 static void     test_none();
 
@@ -44,6 +46,8 @@ typedef uint8_t* (func_encoder)(uint8_t *data, uint64_t data_length, uint64_t *o
 typedef uint8_t* (func_decoder)(uint8_t *data, uint64_t data_length, uint64_t *out_length);
 typedef void (func_test)();
 
+/* This struct defines a format - the name, an encoder, decoder, and a test
+ * routine. */
 typedef struct {
   char *name;
   func_encoder *encoder;
@@ -51,6 +55,8 @@ typedef struct {
   func_test *tester;
 } format_t;
 
+/* A list of format types. To add a new type, add the function to this list,
+ * then to the two strings below. */
 static format_t formats[] = {
   {"none",      encode_none,      NULL,        test_none},
   {"raw",       encode_raw,       decode_raw,  test_raw},
@@ -66,6 +72,8 @@ static format_t formats[] = {
 const char *encode_formats = "none, raw, hex, html, html-pure, cstr, cstr-pure, fancy";
 const char *decode_formats = "raw, hex, html, cstr";
 
+/* A simple function to get the format by the name, rather than searching
+ * manually in code every time. */
 static format_t *format_get_by_name(char *name)
 {
   int i;
@@ -79,11 +87,7 @@ static format_t *format_get_by_name(char *name)
 
 BOOL format_exists(char *format_name)
 {
-  int i;
-  for(i = 0; formats[i].name; i++)
-    if(!strcmp(formats[i].name, format_name))
-      return TRUE;
-  return FALSE;
+  return format_get_by_name(format_name) != NULL;
 }
 
 uint8_t *format_encode(char *format_name, uint8_t *data, uint64_t data_length, uint64_t *out_length)
@@ -104,11 +108,14 @@ uint8_t *format_decode(char *format_name, uint8_t *data, uint64_t data_length, u
     return NULL;
 }
 
+/* Convert a single hex digit (encoded in ascii) at the start of 'hex' to the
+ * actual value. */
 static uint8_t hex_to_int(uint8_t *hex)
 {
   /* These are defined as ints because cygwin. */
   int digit1 = hex[0];
   int digit2 = hex[1];
+
   return (uint8_t)
            ((isdigit(digit1) ? (digit1 - '0') : (tolower(digit1) - 'a' + 10)) << 4) |
            ((isdigit(digit2) ? (digit2 - '0') : (tolower(digit2) - 'a' + 10)) << 0);
@@ -127,11 +134,10 @@ static void test_hex_to_int()
   }
 }
 
-static uint8_t *encode_none(uint8_t *data, uint64_t data_length, uint64_t *out_length)
-{
-  *out_length = 0;
-  return malloc(0);
-}
+static uint8_t *encode_none(uint8_t *data, uint64_t data_length, uint64_t *out_length) {
+  /* Encoding the type 'none' is so easy it's basically cheating. Simply return
+   * a 0-length string. */
+  *out_length = 0; return malloc(0); }
 
 static void test_none()
 {
@@ -156,6 +162,7 @@ static void test_none()
 
 static uint8_t *encode_raw(uint8_t *data, uint64_t data_length, uint64_t *out_length)
 {
+  /* Create a new string and copy the data into it. */
   uint8_t *result = malloc(data_length);
   memcpy(result, data, data_length);
   *out_length = data_length;
@@ -209,16 +216,19 @@ static uint8_t *encode_html(uint8_t *data, uint64_t data_length, uint64_t *out_l
 
   for(i = 0; i < data_length; i++)
   {
-    if(isalpha(data[i]) || isdigit(data[i]))
+    if(isalnum(data[i]))
     {
+      /* If the character is alphanumeric, add it as-is. */
       buffer_add_int8(b, data[i]);
     }
     else if(data[i] == ' ')
     {
+      /* If the character is a space, add a '+'. */
       buffer_add_int8(b, '+');
     }
     else
     {
+      /* Otherwise, encode it as a % followed by a hex code. */
       sprintf(tmp, "%%%02x", data[i]);
       buffer_add_string(b, tmp);
     }
@@ -234,7 +244,10 @@ static uint8_t *decode_html(uint8_t *data, uint64_t data_length, uint64_t *out_l
 
   while(i < data_length)
   {
-    /* The typecasts to 'int' here are to fix warnings from cygwin. */
+    /* If the character is a '%' and we aren't at the end of the string, decode
+     * the hex character and add it to the string.
+     *
+     * The typecasts to 'int' here are to fix warnings from cygwin. */
     if(data[i] == '%' && (i + 2) < data_length && isxdigit((int)data[i + 1]) && isxdigit((int)data[i + 2]))
     {
       /* Add the new character to the string as a uint8_t. */
@@ -302,6 +315,28 @@ static void test_html()
     test_check_memory("decode_html", expected_data, expected_length, decoded_data, decoded_length);
     free(decoded_data);
   }
+
+  /* Check some sidecases (the '%' is at the end of the string). */
+  decoded_data = decode_html((uint8_t*)"%", 1, &decoded_length);
+  test_check_memory("decode_html", (uint8_t*)"%", 1, decoded_data, decoded_length);
+  free(decoded_data);
+
+  decoded_data = decode_html((uint8_t*)"%2", 2, &decoded_length);
+  test_check_memory("decode_html", (uint8_t*)"%2", 2, decoded_data, decoded_length);
+  free(decoded_data);
+
+  decoded_data = decode_html((uint8_t*)"%25", 3, &decoded_length);
+  test_check_memory("decode_html", (uint8_t*)"%", 1, decoded_data, decoded_length);
+  free(decoded_data);
+
+  /* Check other sidecases (the string contains non-hex characters). */
+  decoded_data = decode_html((uint8_t*)"%2g", 3, &decoded_length);
+  test_check_memory("decode_html", (uint8_t*)"%2g", 3, decoded_data, decoded_length);
+  free(decoded_data);
+
+  decoded_data = decode_html((uint8_t*)"%g2", 3, &decoded_length);
+  test_check_memory("decode_html", (uint8_t*)"%g2", 3, decoded_data, decoded_length);
+  free(decoded_data);
 }
 
 static uint8_t *encode_html_pure(uint8_t *data, uint64_t data_length, uint64_t *out_length)
@@ -310,6 +345,7 @@ static uint8_t *encode_html_pure(uint8_t *data, uint64_t data_length, uint64_t *
   buffer_t *b = buffer_create(BO_HOST);
   char tmp[16];
 
+  /* Encode every character as %xx. */
   for(i = 0; i < data_length; i++)
   {
     sprintf(tmp, "%%%02x", data[i]);
@@ -345,6 +381,7 @@ static uint8_t *encode_hex(uint8_t *data, uint64_t data_length, uint64_t *out_le
   buffer_t *b = buffer_create(BO_HOST);
   char tmp[16];
 
+  /* Encode every character as 2 digits of hex. */
   for(i = 0; i < data_length; i++)
   {
     sprintf(tmp, "%02x", data[i]);
@@ -359,8 +396,17 @@ static uint8_t *decode_hex(uint8_t *data, uint64_t data_length, uint64_t *out_le
   buffer_t *b = buffer_create(BO_HOST);
   uint64_t i = 0;
 
+  /* If we wind up with an odd number of characters, the final character is
+   * ignored. */
   while(i + 1 < data_length)
   {
+    /* Skip over and ignore non-hex digits. */
+    if(!isxdigit(data[i]) || !isxdigit(data[i+1]))
+    {
+      i++;
+      continue;
+    }
+
     /* Add the new character to the string as a uint8_t. */
     buffer_add_int8(b, hex_to_int(&data[i]));
 
@@ -400,6 +446,20 @@ static void test_hex()
     test_check_memory("decode_hex", expected_data, expected_length, decoded_data, decoded_length);
     free(decoded_data);
   }
+
+  /* Side-case: odd numbers of hex digits. */
+  decoded_data = decode_hex((uint8_t*)"41424", 5, &decoded_length);
+  test_check_memory("decode_hex ('41424')", (uint8_t*)"AB", 2, decoded_data, decoded_length);
+  free(decoded_data);
+
+  /* Side-case: non-hex characters in the string. */
+  decoded_data = decode_hex((uint8_t*)"414z4", 5, &decoded_length);
+  test_check_memory("decode_hex ('414z4')", (uint8_t*)"A", 1, decoded_data, decoded_length);
+  free(decoded_data);
+
+  decoded_data = decode_hex((uint8_t*)"4141z4141", 9, &decoded_length);
+  test_check_memory("decode_hex ('4141z4141')", (uint8_t*)"AAAA", 4, decoded_data, decoded_length);
+  free(decoded_data);
 }
 
 static uint8_t *encode_cstr(uint8_t *data, uint64_t data_length, uint64_t *out_length)
@@ -410,12 +470,14 @@ static uint8_t *encode_cstr(uint8_t *data, uint64_t data_length, uint64_t *out_l
 
   for(i = 0; i < data_length; i++)
   {
-    if(isalpha(data[i]) || isdigit(data[i]))
+    if(isalnum(data[i]))
     {
+      /* Add letters/numbers as-is. */
       buffer_add_int8(b, data[i]);
     }
     else
     {
+      /* Encode all other characters as "\xNN". */
       sprintf(tmp, "\\x%02x", data[i]);
       buffer_add_string(b, tmp);
     }
@@ -438,7 +500,12 @@ static uint8_t *decode_cstr(uint8_t *data, uint64_t data_length, uint64_t *out_l
       i++;
 
       /* Check for the various format specifiers - \a, \b, \t, \n, \r, etc) */
-      if(i < in_length && data[i] == 'a')
+      if(i < in_length && data[i] == '\\')
+      {
+        buffer_add_int8(b, '\\');
+        i++;
+      }
+      else if(i < in_length && data[i] == 'a')
       {
         buffer_add_int8(b, 0x07);
         i++;
@@ -478,6 +545,7 @@ static uint8_t *decode_cstr(uint8_t *data, uint64_t data_length, uint64_t *out_l
         buffer_add_int8(b, 0x1b);
         i++;
       }
+      /* Ensure the data is sane. */
       else if(i + 2 < in_length && data[i] == 'x' && isxdigit((int)data[i + 1]) && isxdigit((int)data[i + 2]))
       {
         /* Add the new character to the string as a uint8_t. */
@@ -539,6 +607,57 @@ static void test_cstr()
     test_check_memory("decode_cstr", expected_data, expected_length, decoded_data, decoded_length);
     free(decoded_data);
   }
+
+  /* A slash with no character following. */
+  decoded_data = decode_cstr((uint8_t*)"\\", 1, &decoded_length);
+  test_check_memory("decode_cstr ('\\')", (uint8_t*)"\\", 1, decoded_data, decoded_length);
+  free(decoded_data);
+
+  /* \x with no character following. */
+  decoded_data = decode_cstr((uint8_t*)"\\x", 2, &decoded_length);
+  test_check_memory("decode_cstr ('\\x')", (uint8_t*)"\\x", 2, decoded_data, decoded_length);
+  free(decoded_data);
+
+  /* \x with one character following. */
+  decoded_data = decode_cstr((uint8_t*)"\\x1", 3, &decoded_length);
+  test_check_memory("decode_cstr ('\\x1')", (uint8_t*)"\\x1", 3, decoded_data, decoded_length);
+  free(decoded_data);
+
+  /* \b with a 3 after. */
+  decoded_data = decode_cstr((uint8_t*)"\\b3", 3, &decoded_length);
+  test_check_memory("decode_cstr ('\\b3')", (uint8_t*)"\x08""3", 2, decoded_data, decoded_length);
+  free(decoded_data);
+
+  /* \x with an improper hex code. */
+  decoded_data = decode_cstr((uint8_t*)"\\x1z", 4, &decoded_length);
+  test_check_memory("decode_cstr ('\\x1z')", (uint8_t*)"\\x1z", 4, decoded_data, decoded_length);
+  free(decoded_data);
+
+  /* \x with one character following. */
+  decoded_data = decode_cstr((uint8_t*)"\\x1", 3, &decoded_length);
+  test_check_memory("decode_cstr ('\\x1')", (uint8_t*)"\\x1", 3, decoded_data, decoded_length);
+  free(decoded_data);
+
+  /* All the special escape codes. */
+  decoded_data = decode_cstr((uint8_t*)"\\\\\\a\\b\\t\\n\\v\\f\\r\\e", 18, &decoded_length);
+  test_check_memory("decode_cstr ('\\\\\\a\\b\\t\\n\\v\\f\\r\\e')", (uint8_t*)"\\\x07\x08\x09\x0a\x0b\x0c\x0d\x1b", 9, decoded_data, decoded_length);
+  free(decoded_data);
+}
+
+static uint8_t *encode_cstr_pure(uint8_t *data, uint64_t data_length, uint64_t *out_length)
+{
+  int i;
+  buffer_t *b = buffer_create(BO_HOST);
+  char tmp[16];
+
+  /* Encode every character as \xNN. */
+  for(i = 0; i < data_length; i++)
+  {
+    sprintf(tmp, "\\x%02x", data[i]);
+    buffer_add_string(b, tmp);
+  }
+
+  return buffer_create_string_and_destroy(b, out_length);
 }
 
 static void test_cstr_pure()
@@ -561,28 +680,10 @@ static void test_cstr_pure()
   }
 }
 
-static uint8_t *encode_cstr_pure(uint8_t *data, uint64_t data_length, uint64_t *out_length)
-{
-  int i;
-  buffer_t *b = buffer_create(BO_HOST);
-  char tmp[16];
+/* A helper functdion for encode_fancy. */
+#define get_character_from_byte(b) ((b < 0x20 || b > 0x7f) ? '.' : b)
 
-  for(i = 0; i < data_length; i++)
-  {
-    sprintf(tmp, "\\x%02x", data[i]);
-    buffer_add_string(b, tmp);
-  }
-
-  return buffer_create_string_and_destroy(b, out_length);
-}
-
-static char get_character_from_byte(uint8_t byte)
-{
-	if(byte < 0x20 || byte > 0x7F)
-		return '.';
-	return byte;
-}
-
+/* Note: This function isn't tested, so be careful about messing around! */
 static uint8_t *encode_fancy(uint8_t *data, uint64_t data_length, uint64_t *out_length)
 {
   uint64_t i, j;
@@ -610,10 +711,10 @@ static uint8_t *encode_fancy(uint8_t *data, uint64_t data_length, uint64_t *out_
 
   if((i % 16) == 0)
   {
-      sprintf(tmp, "   ");
-      buffer_add_string(b, tmp);
-      for(j = 16; j > 0; j--)
-        buffer_add_int8(b, get_character_from_byte(data[i - j]));
+    sprintf(tmp, "   ");
+    buffer_add_string(b, tmp);
+    for(j = 16; j > 0; j--)
+      buffer_add_int8(b, get_character_from_byte(data[i - j]));
   }
   else
   {
--- formats.h
+++ formats.h
@@ -1,3 +1,44 @@
+/* formats.h By Ron Bowes Created September/2012
+ *
+ * See LICENSE.txt
+ *
+ * This function implements encoders/decoders of various formats. It was
+ * originally written for the hash_extender tool, but is generic enough to be
+ * used elsewhere.
+ *
+ * As a programmer, you basically call format_encode() to encode a string in a
+ * particular format, or format_decode() to convert it back. Both of these
+ * functions take a string representing the format name, as well as the
+ * incoming data and length. It returns an allocated string containing the
+ * encoded or decoded data, and returns the string's length in the final
+ * argument. Be sure to free() the variable.
+ *
+ * A user-readable list of encoders and decoders can be accessed through the
+ * variables below, and format_exists() can be used to verify whether or not an
+ * encoder is valid.
+ *
+ * If you want to add your own encoder/decoder, it's pretty easy. Add the
+ * functions to formats.c, update the 'formats' array at the top, and add it to
+ * the encode_formats/decode_formats string as applicable. That's it!  I highly
+ * recommend using the buffer module to build the strings, so you don't have to
+ * worry about messing around with building strings by hand.
+ *
+ * The formats implemented are:
+ * - none - output nothing in any case
+ * - raw - output the string as-is
+ * - hex - the string is encoded in hexadecimal (eg, "AAA" -> "414141"). This
+ *   is useful for hashes, for instance, which are generally represented this
+ *   way.
+ * - html - non-alphanumeric characters are encoded in html style and spaces
+ *   are replaced with '+' - eg, '!a %z' -> '%21a+%25z'
+ * - html-pure - all characters are encoded in html style - eg. 'ABC' ->
+ *   '%41%42%43'
+ * - cstr - non-alphanumeric characters are encoded in cstring style - eg, '!a
+ *   %z' -> '\x21a\x20\x25z'
+ * - cstr-pure - all characters are encoded - cstr style - eg. 'ABC' ->
+ *   '\x41\x42\x43'
+ */
+
 #include <stdint.h>
 #include "util.h"
 
@@ -7,8 +48,15 @@ extern const char *encode_formats;
 /* A comma-separated (and user-readable) list of decoders. */
 extern const char *decode_formats;
 
+/* Check if the format exists. */
 BOOL     format_exists(char *format);
+
+/* Encode the data and return it in a newly allocated string. */
 uint8_t *format_encode(char *format_name, uint8_t *data, uint64_t data_length, uint64_t *out_length);
+
+/* Decode the data and return it in a newly allocated string. */
 uint8_t *format_decode(char *format_name, uint8_t *data, uint64_t data_length, uint64_t *out_length);
 
+/* Perform self-tests. */
 void format_test();
+
