efc117e151a93b7eb6c037cd6b1429c151bc0fae
Mak Kolybabi <mak@kolybabi.com>
2012-09-23 21:48:39 -0500

    Fix indentation.

--- hash_extender_engine.c
+++ hash_extender_engine.c
@@ -479,14 +479,14 @@ static void sha512_hash(uint8_t *data, uint64_t length, uint8_t *buffer, uint8_t
     for(i = 0; i < state_size; i++)
       SHA512_Update(&c, "A", 1);
 
-      c.h[0] = htobe64(((uint64_t*)state)[0]);
-      c.h[1] = htobe64(((uint64_t*)state)[1]);
-      c.h[2] = htobe64(((uint64_t*)state)[2]);
-      c.h[3] = htobe64(((uint64_t*)state)[3]);
-      c.h[4] = htobe64(((uint64_t*)state)[4]);
-      c.h[5] = htobe64(((uint64_t*)state)[5]);
-      c.h[6] = htobe64(((uint64_t*)state)[6]);
-      c.h[7] = htobe64(((uint64_t*)state)[7]);
+    c.h[0] = htobe64(((uint64_t*)state)[0]);
+    c.h[1] = htobe64(((uint64_t*)state)[1]);
+    c.h[2] = htobe64(((uint64_t*)state)[2]);
+    c.h[3] = htobe64(((uint64_t*)state)[3]);
+    c.h[4] = htobe64(((uint64_t*)state)[4]);
+    c.h[5] = htobe64(((uint64_t*)state)[5]);
+    c.h[6] = htobe64(((uint64_t*)state)[6]);
+    c.h[7] = htobe64(((uint64_t*)state)[7]);
   }
 
   SHA512_Update(&c, data, length);
